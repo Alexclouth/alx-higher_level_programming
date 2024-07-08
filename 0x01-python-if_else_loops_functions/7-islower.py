d39b7bbb10c4a9bc3fd9f42df9dc0f5819f08858
Alexclouth <alazeralphilo@example.com>
2024-07-08 22:21:33 +0300

    d

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    c = str(c)
-    char1 = ord(c)
-    if char1 > 96 and char1 < 124:
+    if ord(c) > 96:
         return True
     else:
         return False
